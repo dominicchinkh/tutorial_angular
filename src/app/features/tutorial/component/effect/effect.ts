3c7449f33c648f6f34838b762b920790a42cecfa
Dominic Chin <dominicchinkh@gmail.com>
2026-05-04 22:25:42 +1000

    Update signal and effect

--- src/app/features/tutorial/component/effect/effect.ts
+++ src/app/features/tutorial/component/effect/effect.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, PLATFORM_ID, computed, effect, signal } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, afterRenderEffect, computed, effect, signal, untracked } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
 @Component({
@@ -11,14 +11,25 @@ export class Effect {
   theme = signal<'light' | 'dark'>('light');
   themeClass = computed(() => `theme-${this.theme()}`);
 
+  count = signal(0)
+
   isLoggedIn = signal(false);
   username = signal('Guest');
 
-  // localStorage is a browser-only API, it does not exist in the Node.js environment 
-  // where the code runs during the initial server-side render
   constructor(@Inject(PLATFORM_ID) private platformId: object) {
 
+    // Effects always run at least once
+
+    // Effects always execute asynchronously, during the change detection process
+
+    // Effects run before the Angular updates the DOM
+
+    //-------------------------------------------------
     // Save theme to local storage whenever it changes
+
+    // localStorage is a browser-only API, it does not exist in the Node.js environment 
+    // where the code runs during the initial server-side render
+    
     effect(() => {
       // Ensure the code only runs when the application is active in the user's browser
       if (isPlatformBrowser(this.platformId)) {
@@ -27,10 +38,69 @@ export class Effect {
       }
     });
 
+    //---------------------------
     // Log user activity changes
-    effect(() => {
+    effect((onCleanup) => {
       const status = this.isLoggedIn()? 'Logged in': 'Logged out';
-      console.log(`User is ${status}`);
+
+      // `untracked` prevent a signal read from `count` being tracked by calling its getter 
+      console.log(`User is ${status}`, untracked(this.count));
+
+      // When you create an effect, your function can optionally accept an `onCleanup` function 
+      // as its first parameter. This `onCleanup` function lets you register a callback that is 
+      // invoked before the next run of the effect begins, or when the effect is destroyed.
+      onCleanup(() => {
+        console.log('Cleaning up...');
+      })
+    });
+
+    //---------------------------------------------------------------------------------
+    // The reactive context is only active for synchronous code. Any signal reads that 
+    // occur after an asynchronous boundary will not be tracked as dependencies.
+
+    let e = effect(async () => {
+      // To ensure all signal reads are tracked, read signals before the await. This includes 
+      // passing them as arguments to the awaited function, since arguments are evaluated synchronously
+      console.log(`User is using theme: ${this.theme()}`)
+
+      await new Promise(() => {
+        setTimeout(() => {}, 2000);
+      });
+      
+      // Reactive context is lost here - theme() won't be tracked
+      console.log(`User is using theme: ${this.theme()}`)
+    });
+
+    // e.destroy();
+
+    //------------------------------
+    // Side effects on DOM elements
+    // `afterRenderEffect` run after Angular has finished rendering and committed its changes to the DOM.
+
+    // https://angular.dev/guide/signals/effect#side-effects-on-dom-elements
+
+    // You often don't need `afterRenderEffext` to check for DOM changes. APIs like ResizeObservar, 
+    // MutationObserver and IntersectionObserver are preferred to `effect` or `aterRenderEffect` 
+    // when possible
+
+    // Example: Create a chart instance
+
+    afterRenderEffect({
+
+      // Example: Update chart data
+
+      // Read from the DOM before a subsequent write callback
+      earlyRead: (cleanupFn) => {},
+
+      // Write to the DOM
+      write: (previousPhaseValue, cleanupFn) => {},
+
+      // Read from and write to the DOM
+      // If you don't specify the phase, `afterRendererEffect` runs callback during this phase
+      mixedReadWrite: (previousPhaseValue, cleanupFn) => {}, 
+
+      // Read from the DOm
+      read: (previousPhaseValue, cleanupFn) => {}
     });
   }
 
@@ -46,5 +116,9 @@ export class Effect {
   logout() {
     this.isLoggedIn.set(false);
     this.username.set('Guest');
+  }
+
+  increaseCount() {
+    this.count.update((value) => { return value + 1; });
   }
 }
--- src/app/features/tutorial/component/signal/signal.ts
+++ src/app/features/tutorial/component/signal/signal.ts
@@ -1,12 +1,18 @@
 import { 
   ChangeDetectionStrategy, 
-  Component, 
+  Component,
+  Resource,
+  ResourceSnapshot,
   linkedSignal,
   resource,
+  resourceFromSnapshots,
   signal, 
   computed 
 } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import isEqual from 'lodash/isEqual';
 import { getUserData } from './user-api';
+import { User } from '../../model/user';
 
 @Component({
   selector: 'app-signal',
@@ -53,12 +59,93 @@ export class Signal {
   //-----------
   // Resource: To simplify asynchronous data fetching and state management using Signals
   userResource = resource({
+
+    // The param value recomputes whenever any read signals change
     params: () => ({ id: this.userId() }),
-    loader: (params) => getUserData(params.params.id)
+
+    // The resource calls this function every time the `params` value changes
+
+    // If the `params` computation returns `undefined`, the loader function does not run and 
+    // the resource status become `idle`
+
+    // More on `AbortSignal`: https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal
+    loader: ({params, abortSignal}) => getUserData(params.id)
   });
 
+  // https://angular.dev/guide/signals/resource#resource-status
   isLoading = computed(() => this.userResource.status() === 'loading');
   hasError  = computed(() => this.userResource.status() === 'error');
+
+  constructor() {
+
+    //--------------------------
+    // Signal equality function
+
+    // https://angular.dev/guide/signals#signal-equality-functions
+
+    const data = signal(['test'], {equal: isEqual});
+
+    // Even though this is a different array instance, the deep equality
+    // function will consider the values to be equal, and the signal won't
+    // trigger any updates.
+    
+    // By default, signals use referential equality (Object.is() comparison).
+    data.set(['test']);
+
+    //-------------------------------------------------------
+    // `linkedSignal` with a separate source and computation
+
+    // https://angular.dev/guide/signals/linked-signal#accounting-for-previous-state
+
+    // The following example shows how the value of a linkedSignal can change based on its linked state
+    const users = signal<User[]>([
+      { id: 0, name: 'Sarah'    },
+      { id: 1, name: 'Amy'      },
+      { id: 2, name: 'Rachel'   },
+      { id: 3, name: 'Jessica'  },
+      { id: 4, name: 'Poornima' }
+    ]);
+    
+    const selectedOption = linkedSignal<User[], User>({
+      
+      // `selectedOption` is set to the `computation` result whenever this `source` changes
+      source: users,
+
+      // If the `newOptions` contain the previously selected option, preserve that selection.
+      // Otherwise, default to the first option.
+      computation: (newOptions, previous) => {
+        return newOptions.find((opt) => opt.id === previous?.value.id)?? newOptions[0]
+      },
+
+      // A custom equality function used by downstream dependencies to determine if that value 
+      // of the linkedSignal (result of a computation) changed:
+      equal: (a, b) => a.name === b.name
+    });
+
+    console.log(selectedOption()); // 'Sarah'
+
+    selectedOption.set(users()[2]); 
+    console.log(selectedOption()); // 'Rachel'
+
+    users.set([      
+      { id: 0, name: 'John'    },
+      { id: 1, name: 'Peter'   },
+      { id: 2, name: 'Andrew'  },
+      { id: 3, name: 'Stephen' }
+    ]);
+    console.log(selectedOption()); // 'Andrew'
+
+    //-----------------------------------
+    // Composing resource with snapshots
+
+    // https://angular.dev/guide/signals/resource#composing-resources-with-snapshots
+
+    // `httpResource` makes a reactive HTTP request and exposes the request status and response 
+    // value as a WritableResource
+
+    // https://angular.dev/guide/signals/resource#reactive-data-fetching-with-httpresource
+    const user = this.withPreviousValue(httpResource(() => `http://localhost:3000/users/${this.userId()}`));
+  }
 
   goOnline() {
     this.userStatus.set('online');
@@ -87,4 +174,22 @@ export class Signal {
   reloadUser() {
     this.userResource.reload();
   }
+
+  withPreviousValue<T>(input: Resource<T>): Resource<T> {
+    const derived = linkedSignal<ResourceSnapshot<T>, ResourceSnapshot<T>>({
+      source: input.snapshot,
+      computation: ((snap, previous) => {
+        if (snap.status === 'loading' && previous && previous.value.status != 'error') {
+          // When the input resource enters loading state, we keep the value from its previous state,
+          // if any
+          return { 'status': 'loading' as const, value: previous.value.value };
+        }
+
+        // Otherwise, we simply forward the state of the input resouce
+        return snap;
+      })
+    })
+
+    return resourceFromSnapshots(derived);
+  }  
 }
